f2612962d1d1de09bfee7cbfcea39b25e1d6aca6
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2021-09-12 02:53:43 -1100

    Add files via upload

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,85 +1,162 @@
 ﻿using System;
 
-namespace POO_exercicio02
+namespace POO_teste
 {
-    public class animal
+    public class prova
     {
         private string nome;
         public string Nome
         {
             get { return nome; }
-            set { nome = value.ToUpper(); }
+            set { nome = value; }
         }
-        private string tipo;
-        public string Tipo //valores Cachorro gato peixe
+
+        private int idade;
+        public int Idade
         {
-            get { return tipo; }
-            set { 
-                if(value == "CACHORRO" || value == "GATO" || value == "PEIXE")
+            get { return idade; }
+            set
+            {
+                while((value < 4) || (value > 100))
                 {
-                    tipo = value;
+                    Console.WriteLine("É bem improvável que faça a prova!");
+                    Console.WriteLine("Digite uma idade autentica:");
+                    value = Convert.ToInt32(Console.ReadLine());
+                }
+                if(value < 18)
+                {
+                    Console.WriteLine("Você não pode fazer a prova!!!");
                 }
                 else
                 {
-                    tipo = "PEIXE";
+                    idade = value;
                 }
             }
         }
-        
+        private double nota;
+        public double Nota
+        {
+            get { return nota; }
+            set
+            {
+                while((value < 1)||(value > 10))
+                {
+                    Console.WriteLine("Não existe essa nota!! Digite outra:");
+                    value = Convert.ToDouble(Console.ReadLine());
+                }
+                if(value >= 7)
+                {
+                    Console.WriteLine("O estudante passou!!!");
+                    nota = value;
+                }
+            }
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            animal a1 = new animal(); //5 objetos
-            animal a2 = new animal();
-            animal a3 = new animal();
-            animal a4 = new animal();
-            animal a5 = new animal();
-            int tc = 0, tg = 0, tp = 0;
+            int passaram = 0, reprovaram = 0, n = 0;
+            Console.WriteLine("\t===========================");
+            Console.WriteLine("\t====Faça o vestibulinho====");
+            Console.WriteLine("\t===========================\n");
 
-            Console.Write("Digite o nome de seu 1º bixo:  ");
+            prova a1 = new prova();
+            prova a2 = new prova();
+            prova a3 = new prova();
+
+            Console.WriteLine("Digite o nome do 1º estudante:");
             a1.Nome = Console.ReadLine();
-            Console.Write("Digite que animal é (Cachorro, Gato ou Peixe):  ");
-            a1.Tipo = Console.ReadLine().ToUpper();
-            if (a1.Tipo == "CACHORRO") tc++;
-            if (a1.Tipo == "GATO") tg++;
-            if (a1.Tipo == "PEIXE") tp++;
+            Console.WriteLine("Digite a idade do 1º estudante:");
+            a1.Idade = Convert.ToInt32(Console.ReadLine());
+            if (a1.Idade >= 18)
+            {
+                Console.WriteLine("Digite a nota do 1º estudante:");
+                a1.Nota = Convert.ToDouble(Console.ReadLine());
+                if (a1.Nota >= 7) passaram++;
+                else reprovaram++;
+            }
+            else n++;
 
-            Console.Write("Digite o nome de seu 2º bixo:  ");
+            Console.WriteLine("Digite o nome do 2º estudante:");
             a2.Nome = Console.ReadLine();
-            Console.Write("Digite que animal é (Cachorro, Gato ou Peixe):  ");
-            a2.Tipo = Console.ReadLine().ToUpper();
-            if (a2.Tipo == "CACHORRO") tc++;
-            if (a2.Tipo == "GATO") tg++;
-            if (a2.Tipo == "PEIXE") tp++;
+            Console.WriteLine("Digite a idade do 2º estudante:");
+            a2.Idade = Convert.ToInt32(Console.ReadLine());
+            if (a2.Idade >= 18)
+            {
+                Console.WriteLine("Digite a nota do 2º estudante:");
+                a2.Nota = Convert.ToDouble(Console.ReadLine());
+                if (a2.Nota >= 7) passaram++;
+                else reprovaram++;
+            }
+            else n++;
 
-            Console.Write("Digite o nome de seu 3º bixo:  ");
+            Console.WriteLine("Digite o nome do 3º estudante:");
             a3.Nome = Console.ReadLine();
-            Console.Write("Digite que animal é (Cachorro, Gato ou Peixe):  ");
-            a3.Tipo = Console.ReadLine().ToUpper();
-            if (a3.Tipo == "CACHORRO") tc++;
-            if (a3.Tipo == "GATO") tg++;
-            if (a3.Tipo == "PEIXE") tp++;
+            Console.WriteLine("Digite a idade do 3º estudante:");
+            a3.Idade = Convert.ToInt32(Console.ReadLine());
+            if (a3.Idade >= 18)
+            {
+                Console.WriteLine("Digite a nota do 3º estudante:");
+                a3.Nota = Convert.ToDouble(Console.ReadLine());
+                if (a3.Nota >= 7) passaram++;
+                else reprovaram++;
+            }
+            else n++;
 
-            Console.Write("Digite o nome de seu 4º bixo:  ");
-            a4.Nome = Console.ReadLine();
-            Console.Write("Digite que animal é (Cachorro, Gato ou Peixe):  ");
-            a4.Tipo = Console.ReadLine().ToUpper();
-            if (a4.Tipo == "CACHORRO") tc++;
-            if (a4.Tipo == "GATO") tg++;
-            if (a4.Tipo == "PEIXE") tp++;
+            if((a1.Nota > a2.Nota) && (a1.Nota > a3.Nota))
+            {
+                Console.WriteLine($"{a1.Nome} teve a maior nota!!");
+                Console.WriteLine($"Nota: {a1.Nota}\n");
+            }
+            else
+            {
+                if((a2.Nota > a1.Nota) && (a2.Nota > a2.Nota))
+                {
+                    Console.WriteLine($"{a2.Nome} teve a maior nota!!");
+                    Console.WriteLine($"Nota: {a2.Nota}\n");
+                }
+                else
+                {
+                    if((a3.Nota > a1.Nota) && (a3.Nota > a2.Nota))
+                    {
+                        Console.WriteLine($"{a3.Nome} teve a maior nota!!");
+                        Console.WriteLine($"Nota: {a3.Nota}\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Os concorrentes tiveram notas iguais!!!");
+                        if((a1.Nota == a2.Nota) && (a1.Nota == a3.Nota))
+                        {
+                            Console.WriteLine($"Nome: {a1.Nome} Nota: {a1.Nota}");
+                            Console.WriteLine($"Nome: {a2.Nome} Nota: {a2.Nota}");
+                            Console.WriteLine($"Nome: {a3.Nome} Nota: {a3.Nota}");
+                        }
+                        else
+                        {
+                            if(a1.Nota == a2.Nota)
+                            {
+                                Console.WriteLine($"Nome: {a1.Nome} Nota: {a1.Nota}");
+                                Console.WriteLine($"Nome: {a2.Nome} Nota: {a2.Nota}");
+                            }
+                            if(a1.Nota == a3.Nota)
+                            {
+                                Console.WriteLine($"Nome: {a1.Nome} Nota: {a1.Nota}");
+                                Console.WriteLine($"Nome: {a3.Nome} Nota: {a3.Nota}");
+                            }
+                            if(a2.Nota == a3.Nota)
+                            {
+                                Console.WriteLine($"Nome: {a2.Nome} Nota: {a2.Nota}");
+                                Console.WriteLine($"Nome: {a3.Nome} Nota: {a3.Nota}");
+                            }
+                        }
+                        
+                    }
+                }
+            }
 
-            Console.Write("Digite o nome de seu 5º bixo:  ");
-            a5.Nome = Console.ReadLine();
-            Console.Write("Digite que animal é (Cachorro, Gato ou Peixe):  ");
-            a5.Tipo = Console.ReadLine().ToUpper();
-            if (a5.Tipo == "CACHORRO") tc++;
-            if (a5.Tipo == "GATO") tg++;
-            if (a5.Tipo == "PEIXE") tp++;
-
-            Console.Write($"Cachorro(s): [{tc}]\nGato(s): [{tg}]\nPeixe(s): [{tp}]\n");
-            Console.ReadKey();
+            Console.WriteLine($"\npassaram {passaram} \nreprovados {reprovaram} ");
+            Console.WriteLine($"Não pode fazer a prova: {n}");
         }
     }
 }
